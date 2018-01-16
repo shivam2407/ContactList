246d07267b2f18245161e8ece46c88a66e780f05
Javesh <shivarawat24@gmail.com>
2018-01-15 23:10:04 -0500

    Rest APi created

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,11 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope','$http',function($scope,$http){
   console.log("Hello World from Controller");
+
+  $http.get('/contactlist').then(function(response){
+    console.log("I got the data");
+    $scope.contactlist = response.data;
+  });
+
+
 }]);
--- server.js
+++ server.js
@@ -2,6 +2,29 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.get('/contactlist',function (req,res){
+  console.log("I recieved a get request");
+  person1 = {
+    name:"Alex",
+    number:"999-232-3443",
+    email:"alex@gmail.com"
+  };
+
+  person2 = {
+    name:"Amili",
+    number:"998-423-4284",
+    email:"amili@gmail.com"
+  };
+
+  person3 = {
+    name:"John",
+    number:"998-364-2232",
+    email:'john@yahoo.in'
+  };
+
+  var contactlist = [person1,person2,person3];
+  res.json(contactlist);
+});
 
 app.listen(3000);
 console.log("Server running on port 3000");
